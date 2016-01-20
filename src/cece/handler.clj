9e60ab79a47f4d3b6a40a4a0b72c4476a1d9e3ec
mjamesruggiero <mjamesruggiero@gmail.com>
2016-01-19 21:30:53 -0800

    Use Liberator

--- src/cece/handler.clj
+++ src/cece/handler.clj
@@ -1,23 +1,71 @@
 (ns cece.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [liberator.core :refer [resource defresource]]
+            [liberator.representation :refer [render-map-generic]]
+            [hiccup.core :refer [html]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.data.json :as json]))
 
-(defn handle-http []
-  (context "/:id" [id]
-           (defroutes api-routes
-             (GET "/" [] (str "get called: " id "\n"))
-             (POST "/" {form-params :form-params}
-                   (str "post called: " id "\n" form-params "\n"))
-             (PUT "/" req (str "put called with params: " req))
-             (DELETE "/" [] (str "delete called: " id "\n")))))
+(def default-media-types
+  ["application/json"
+   "text/plain"
+   "text/html"])
+
+(defmethod render-map-generic "application/json" [data context]
+  (json/write-str (conj (:links data) (:properties data))))
+
+(defmethod render-map-generic "text/html" [data context]
+  (html [:div
+         [:h1 (-> data :class first)]
+         [:d1
+          (mapcat (fn [[key value]] [[:dt key] [:dd value]])
+                  (:properties data))]]))
+
+(defrecord Coffee [name price])
+
+(def ristretto
+  (->Coffee "Ristretto" "$1.80"))
+
+(def macchiato
+  (->Coffee "Macchiato" "$1.80"))
+
+(def latte
+  (->Coffee "Latte" "$3.20"))
+
+(def coffees [latte macchiato ristretto])
+
+(defn index-data [ctx]
+  coffees)
+
+(defresource index
+  :allowed-methods [:options :get :post :delete]
+  :available-media-types default-media-types
+  :handle-ok index-data)
+
+(defresource coffee [id]
+  :allowed-methods [:options :get]
+  :available-media-types ["text/html" "application/json"]
+  :handle-ok (fn [_]
+               {:properties
+                (nth coffees (Integer/parseInt id))
+                :links [{:rel ["self"]
+                         :href (str "/coffees/" id)}
+                        {:rel ["listing"]
+                         :href "/coffees"}]}))
+
 
 (defroutes app-routes
-  (handle-http)
-  (route/not-found (str
-                     "This is the default page - try "
-                     "<a href='http://localhost:4000/33'>this</a>\n")))
+  (OPTIONS "/" []
+           {:headers {"Allow:" "GET, POST, DELETE, OPTIONS"}})
+  (ANY "/" [] index)
+  (ANY "/coffees" [] index)
+  (OPTIONS "/coffees/:id" []
+       {:headers {"Allow:" "GET, OPTIONS"}})
+  (ANY "/coffees/:id" [id]
+       (coffee id))
+  (route/resources "/")
+  (route/not-found "Not found"))
 
 (def app
-  (wrap-defaults app-routes
-                 (assoc-in site-defaults [:security :anti-forgery] false)))
+  (wrap-defaults app-routes site-defaults))
